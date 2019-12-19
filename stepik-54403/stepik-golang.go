5ef82699bec358b08bff14ee3f68a0b5e9ca9b6d
Denis Novikov <voothi@gmail.com>
2019-12-20 02:49:33 +0300

    Переменные https://stepik.org/lesson/228261/step/11?unit=200794 Sample Input: 1 Sample Output: 102

--- stepik-54403/stepik-golang.go
+++ stepik-54403/stepik-golang.go
@@ -1,8 +1,11 @@
 package main
 
+import "fmt"
+
 func main() {
-	name := "Denis"
-	age := 32
-	println("My name is", name, "and I am", age, "years old.")
-	//My name is Denis and I am 32 years old.
+	var a int
+	fmt.Scan(&a)
+	fmt.Print((a * 2) + 100)
+	//102
+	//1
 }
