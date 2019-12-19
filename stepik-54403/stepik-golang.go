abaec38d767ae9fbeb5ee2d2330a8e5396c10159
Denis Novikov <voothi@gmail.com>
2019-12-20 02:41:48 +0300

    Переменные My name is Denis and I am 32 years old.

--- stepik-54403/stepik-golang.go
+++ stepik-54403/stepik-golang.go
@@ -1,16 +1,8 @@
 package main
 
-import "fmt"
-
 func main() {
-	var name string
-	var age int
-	fmt.Print("Введите имя: ")
-	fmt.Scan(&name)
-	fmt.Print("Введите возраст: ")
-	fmt.Scan(&age)
-	fmt.Println(name, age)
-	//Введите имя: Denis
-	//Введите возраст: 32
-	//Denis 32
+	name := "Denis"
+	age := 32
+	println("My name is", name, "and I am", age, "years old.")
+	//My name is Denis and I am 32 years old.
 }
