9fc608fa42e4ab988b7312cb219db18994938518
Denis Novikov <voothi@gmail.com>
2019-12-20 03:07:37 +0300

    Переменные https://stepik.org/lesson/228261/step/13?unit=200794

--- stepik-54403/stepik-golang.go
+++ stepik-54403/stepik-golang.go
@@ -4,12 +4,7 @@ import "fmt"
 
 func main() {
 	var a int
-	var b int
-	var c int
 	fmt.Scan(&a) // считаем переменную 'a' с консоли
-	fmt.Scan(&b) // считаем переменную 'b' с консоли
 	a = a * a
-	b = b * b
-	c = a + b
-	fmt.Println(c)
+	fmt.Println(a)
 }
