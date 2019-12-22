b8a267f707ff4897929e7b03b116688ef1948733
Denis Novikov <voothi@gmail.com>
2019-12-22 20:55:22 +0300

    Переменные https://stepik.org/lesson/228264/step/2?unit=200797

--- stepik-54403/stepik-golang.go
+++ stepik-54403/stepik-golang.go
@@ -3,13 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	var d int
-	fmt.Scan(&d)
-	var time int
-	var h int
-	var m int
-	time = d * 120
-	h = time / 3600
-	m = (time % 3600) / 60
-	fmt.Println("It is", h, "hours", m, "minutes.")
+	var a int = 8
+	const b int = 10
+	a = a + b
+	fmt.Println(a)
 }
